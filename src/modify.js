e8441325766f9fa58bc7463cc1c83aa14b73b39a
RyJaxxon <ryanjaxxon@gmail.com>
2024-05-14 16:55:41 -0400

    Questions 1 and 2 completed

--- src/modify.js
+++ src/modify.js
@@ -1,8 +1,31 @@
+const buttonClick = document.querySelector('#click-button')
 const clickCounterHandler = () => {
+  let nums = buttonClick.dataset.clicks
+  nums++
+  buttonClick.dataset.clicks = nums
+  console.log(nums)
+  if (nums === 1){
+    buttonClick.textContent = `I've been clicked ${nums} time.`
+  } else {
+    buttonClick.textContent = `I've been clicked ${nums} times!`
+  }
 };
+buttonClick.addEventListener("click", clickCounterHandler)
 
-const handleKeydown = () => {
+const keyTracker = document.querySelector('#keydown-tracker')
+const handleKeydown = (e) => {
+  // console.log(e);
+  const keyPressed = e.code
+  if (keyPressed === 'Enter'){
+    keyTracker.textContent = `You pressed Enter`
+  } else if (keyPressed === " "){
+    keyTracker.textContent = `You pressed Space`
+  } else {
+    keyTracker.textContent = `You pressed ${e.code}`
+  }
 };
+document.body.addEventListener("keydown",handleKeydown)
+
 
 const handleDelegation = (event) => {
   const resultSpan = document.querySelector('#delegation-result');
